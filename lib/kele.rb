1130b77493b482abe7055270c302f3d93b17ee8d
Gerrald Mitchell <bluelionjr@gmail.com>
2017-06-28 17:29:10 -0400

    Added JSON gem, retrieve user

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
@@ -11,5 +12,10 @@ class Kele
     response = self.class.post('/sessions', body: {email: email, password: password})
 
     @auth_token = response['auth_token']
+  end
+
+  def get_me
+    response = self.class.get('/users/me', headers: { authorization: @auth_token })
+    JSON.parse(response.body)
   end
 end
