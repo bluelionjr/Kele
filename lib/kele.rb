f1f4c570ef7d0152de2aceec8d4eda50fa156566
Gerrald Mitchell <bluelionjr@gmail.com>
2017-06-29 00:29:09 -0400

    Get mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -6,7 +6,7 @@ class Kele
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
-    @emal = email
+    @email = email
     @password = password
 
     response = self.class.post('/sessions', body: {email: email, password: password})
@@ -16,6 +16,13 @@ class Kele
 
   def get_me
     response = self.class.get('/users/me', headers: { authorization: @auth_token })
-    JSON.parse(response.body)
+    @user_data = JSON.parse(response.body)
+  end
+
+  def get_mentor_availability
+    mentor_id = self.get_me['current_enrollment']['mentor_id']
+    mentor_response = self.class.get('/mentors/2290632/student_availability', headers: { authorization: @auth_token })
+    @mentor_sched = JSON.parse(mentor_response.body)
+    @mentor_sched.to_a
   end
 end
